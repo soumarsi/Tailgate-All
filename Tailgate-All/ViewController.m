f1d21f4d6d68da0f142a3805a2236c10da42e9b8
soumarsi <maxcon9@Soumarsikundu-macAir.local>
2015-06-24 12:18:39 +0530

    Initial Commit

---
+++ Tailgate-All/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tailgate-All
+//
+//  Created by Soumarsi Kundu on 24/06/15.
+//  Copyright (c) 2015 esolz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
